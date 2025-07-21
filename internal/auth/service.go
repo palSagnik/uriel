fa151ecfbbfc79c00486f217e940894c11074b3d
Sagnik <palsagnik2102@gmail.com>
2025-07-22 00:30:26 +0530

    handler service pattern for register route
    Signed-off-by: Sagnik <palsagnik2102@gmail.com>

--- internal/auth/service.go
+++ internal/auth/service.go
@@ -1,17 +1,66 @@
 package auth
 
 import (
-	"net/http"
+	"context"
+	"errors"
+	"fmt"
+	"time"
 
-	"github.com/gin-gonic/gin"
+	"github.com/palSagnik/uriel/internal/models"
+	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/crypto/bcrypt"
 )
 
-// TODO: implement only business logic here
-func signupService(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"message": "ok"})
+type Service struct {
+	repo AuthRepository
 }
 
-func loginService(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"message": "ok"})
+func NewService(repo AuthRepository) *Service {
+	return &Service{repo: repo}
 }
 
+func (s *Service) RegisterPlayerService(ctx context.Context, req *models.RegisterRequest) (*models.Player, error) {
+
+	// check if this username already exists
+	existingPlayerByUsername, err := s.repo.GetPlayerByUsername(ctx, req.Username)
+	if err != nil && err != mongo.ErrNoDocuments {
+		return nil, fmt.Errorf("service: error checking existing username %w", err)
+	}
+	if existingPlayerByUsername != nil {
+		return nil, errors.New("username already exists")
+	}
+
+	// check if this email already exists
+	existingPlayerByEmail, err := s.repo.GetPlayerByEmail(ctx, req.Email)
+	if err != nil && err != mongo.ErrNoDocuments {
+		return nil, fmt.Errorf("service: error checking existing email %w", err)
+	}
+	if existingPlayerByEmail != nil {
+		return nil, errors.New("email already exists")
+	}
+
+	// hash password
+	// does not accept more than 72 bytes
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, fmt.Errorf("service: error hashing password %w", err)
+	}
+
+	// create player
+	// TODO: Role and errors should be ENUMS
+	newPlayer := models.Player{
+		Username: req.Username,
+		Email: req.Email,
+		Password: string(hashedPassword),
+		Role: "player",
+		IsOnline: false,
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+	}
+
+	if err := s.repo.CreatePlayer(ctx, newPlayer); err != nil {
+		return nil, fmt.Errorf("service: error in creating new player %w", err)
+	}
+
+	return &newPlayer, nil
+}
